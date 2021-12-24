5f1e0234bcfe8c6bb2e69580042224aa23bf0c05
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2021-12-24 13:59:29 +0530

    Update language.py

--- sophie_bot/modules/utils/language.py
+++ sophie_bot/modules/utils/language.py
@@ -1,87 +1,87 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-# Copyright (C) 2018 - 2019 MrYacha
-#
-# This file is part of SophieBot.
-#
-# SophieBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-
 import os
 
 import yaml
 from babel.core import Locale
 
-from sophie_bot.services.mongo import db
-from sophie_bot.services.redis import redis
-from sophie_bot.utils.logger import log
+from DaisyX.services.mongo import db
+from DaisyX.services.redis import redis
+from DaisyX.utils.logger import log
 
 LANGUAGES = {}
 
 log.info("Loading localizations...")
 
-for filename in os.listdir('sophie_bot/localization'):
-    log.debug('Loading language file ' + filename)
-    f = open('sophie_bot/localization/' + filename, "r", encoding='utf8')
-    lang = yaml.load(f, Loader=yaml.CLoader)
+for filename in os.listdir("sophie_bot/localization"):
+    log.debug("Loading language file " + filename)
+    with open("sophie_bot/localization/" + filename, "r", encoding="utf8") as f:
+        lang = yaml.load(f, Loader=yaml.CLoader)
 
-    lang_code = lang['language_info']['code']
-    lang['language_info']['babel'] = Locale(lang_code)
+        lang_code = lang["language_info"]["code"]
+        lang["language_info"]["babel"] = Locale(lang_code)
 
-    LANGUAGES[lang_code] = lang
+        LANGUAGES[lang_code] = lang
 
-log.info("Languages loaded: {}".format(
-    [l['language_info']['babel'].display_name for l in LANGUAGES.values()]))
+log.info(
+    "Languages loaded: {}".format(
+        [
+            language["language_info"]["babel"].display_name
+            for language in LANGUAGES.values()
+        ]
+    )
+)
 
 
 async def get_chat_lang(chat_id):
-    r = redis.get('lang_cache_{}'.format(chat_id))
+    r = redis.get("lang_cache_{}".format(chat_id))
     if r:
         return r
     else:
-        db_lang = await db.lang.find_one({'chat_id': chat_id})
+        db_lang = await db.lang.find_one({"chat_id": chat_id})
         if db_lang:
             # Rebuild lang cache
-            redis.set('lang_cache_{}'.format(chat_id), db_lang['lang'])
-            return db_lang['lang']
-        user_lang = await db.user_list.find_one({'user_id': chat_id})
-        if user_lang and user_lang['user_lang'] in LANGUAGES:
+            redis.set("lang_cache_{}".format(chat_id), db_lang["lang"])
+            return db_lang["lang"]
+        user_lang = await db.user_list.find_one({"user_id": chat_id})
+        if user_lang and user_lang["user_lang"] in LANGUAGES:
             # Add telegram language in lang cache
-            redis.set('lang_cache_{}'.format(chat_id), user_lang['user_lang'])
-            return user_lang['user_lang']
+            redis.set("lang_cache_{}".format(chat_id), user_lang["user_lang"])
+            return user_lang["user_lang"]
         else:
-            return 'en'
+            return "en"
 
 
 async def change_chat_lang(chat_id, lang):
-    redis.set('lang_cache_{}'.format(chat_id), lang)
-    await db.lang.update_one({'chat_id': chat_id}, {"$set": {'chat_id': chat_id, 'lang': lang}}, upsert=True)
+    redis.set("lang_cache_{}".format(chat_id), lang)
+    await db.lang.update_one(
+        {"chat_id": chat_id}, {"$set": {"chat_id": chat_id, "lang": lang}}, upsert=True
+    )
 
 
 async def get_strings(chat_id, module, mas_name="STRINGS"):
     chat_lang = await get_chat_lang(chat_id)
     if chat_lang not in LANGUAGES:
-        await change_chat_lang(chat_id, 'en')
+        await change_chat_lang(chat_id, "en")
 
     class Strings:
-        def get_strings(self, lang, mas_name, module):
+        @staticmethod
+        def get_strings(lang, mas_name, module):
 
-            if mas_name not in LANGUAGES[lang] or module not in LANGUAGES[lang][mas_name]:
+            if (
+                mas_name not in LANGUAGES[lang]
+                or module not in LANGUAGES[lang][mas_name]
+            ):
                 return {}
 
             data = LANGUAGES[lang][mas_name][module]
 
-            if mas_name == 'STRINGS':
-                data['language_info'] = LANGUAGES[chat_lang]['language_info']
+            if mas_name == "STRINGS":
+                data["language_info"] = LANGUAGES[chat_lang]["language_info"]
             return data
 
         def get_string(self, name):
             data = self.get_strings(chat_lang, mas_name, module)
             if name not in data:
-                data = self.get_strings('en', mas_name, module)
+                data = self.get_strings("en", mas_name, module)
 
             return data[name]
 
@@ -100,9 +100,9 @@ def get_strings_dec(module, mas_name="STRINGS"):
     def wrapped(func):
         async def wrapped_1(*args, **kwargs):
             message = args[0]
-            if hasattr(message, 'chat'):
+            if hasattr(message, "chat"):
                 chat_id = message.chat.id
-            elif hasattr(message, 'message'):
+            elif hasattr(message, "message"):
                 chat_id = message.message.chat.id
             else:
                 chat_id = None
@@ -117,4 +117,4 @@ def get_strings_dec(module, mas_name="STRINGS"):
 
 async def get_chat_lang_info(chat_id):
     chat_lang = await get_chat_lang(chat_id)
-    return LANGUAGES[chat_lang]['language_info']
+    return LANGUAGES[chat_lang]["language_info"]
